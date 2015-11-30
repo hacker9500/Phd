ad9e0d6ce3e9c93f560cc28041a3f628d853983e
shubham-xtdevo <shubham14100@iiitd.ac.in>
2015-11-30 22:34:39 +0530

    save2 cmpleted

--- src/com/iiitd/Person.java
+++ src/com/iiitd/Person.java
@@ -227,6 +227,14 @@ public class Person {
 			}
 	}
 	public static int s2(HttpServletRequest req, HttpServletResponse resp){
+		String marksField = null;
+		String cgpaField = null;
+		String chk1 = null;
+		String chk2 = null;
+		String chk3 = null;
+		String chk4 = null;
+		String phdMARKS = null;
+		String phdCGPA = null;
 		try{
 			xBoard = req.getParameter("xBoard");
 			xMarks = req.getParameter("xMarks");
@@ -234,8 +242,266 @@ public class Person {
 			xiiBoard = req.getParameter("xiiBoard");
 			xiiMarks = req.getParameter("xiiMarks");
 			xiiYear = req.getParameter("xiiYear");
-			return 0;
+			gradDegree = req.getParameter("gradDegree");
+			gradDepartment = req.getParameter("gradDepartment");
+			gCollege = req.getParameter("gCollege");
+			gUniversity = req.getParameter("gUniversity");
+			gCity = req.getParameter("gCity");
+			gState = req.getParameter("gState");
+			gYear = req.getParameter("gYear");
+			marksField = req.getParameter("Marks");
+			cgpaField = req.getParameter("CGPA");
+			// Applying for ECE
+			if(req.getParameterValues("chk1") == null){
+				chk1 = req.getParameterValues("chk1")[0];
+				ecePref1 = req.getParameter("ecePref1");
+				ecePref2 = req.getParameter("ecePref2");
+				ecePref3 = req.getParameter("ecePref3");
+				ecePref4 = req.getParameter("ecePref4");
+			}
+			if(req.getParameterValues("chk2") == null){
+				chk2 = req.getParameterValues("chk2")[0];
+				phdCollege  = req.getParameter("phdCollege");
+				phdCity = req.getParameter("phdCity");
+				phdState = req.getParameter("phdState");
+				phdDepartment = req.getParameter("phdDepartment");
+				phdDegree = req.getParameter("phdDegree");
+				phdTitle = req.getParameter("phdTitle");
+				phdYear = req.getParameter("phdYear");
+				phdMarks = req.getParameter("phdMarks");
+				
+			}
+			if(req.getParameterValues("chk3") == null){
+				chk3 = req.getParameterValues("chk3")[0];
+				examName = req.getParameter("examName");
+				examSubject = req.getParameter("examSubject");
+				examYear = req.getParameter("examYear");
+				examScore = req.getParameter("examScore");
+				examRank = req.getParameter("examRank");
+			}
+			if(req.getParameterValues("chk4") == null){
+				chk4 = req.getParameterValues("chk4")[0];
+				gateArea = req.getParameter("gateArea");
+				gateYear = req.getParameter("gateYear");
+				gateMarks = req.getParameter("gateMarks");
+				gateScore = req.getParameter("gateScore");
+				gateRank = req.getParameter("gateRank");
+			}
 		}catch(Exception ex){return 1;}
+		
+		try{
+		if(xBoard.isEmpty()||xMarks.isEmpty()||xiiBoard.isEmpty()||xiiMarks.isEmpty()||gradDegree.isEmpty()||gradDepartment.isEmpty()||gCollege.isEmpty()|| gUniversity.isEmpty()|| gCity.isEmpty())
+			return 1;
+		else if(gState.isEmpty()||gYear.isEmpty() || xYear.isEmpty()||xiiYear.isEmpty())
+			return 1;
+//		else if(cvText1.equals("No File chosen") || statePurposeTxt1.equals("No File chosen"))
+//			return 1;
+		else{
+			try{
+				try{
+					if(Double.parseDouble(xMarks) > 100 || Double.parseDouble(xMarks) < 0 || Double.parseDouble(xiiMarks)>100 || Double.parseDouble(xiiMarks)<0){
+						return 1;
+					}
+					}catch(Exception e){
+						return 1;
+					}
+				if(chk1 != null && !chk1.isEmpty()){
+					if(ecePref1.isEmpty()|| ecePref2.isEmpty()||ecePref3.isEmpty()||ecePref4.isEmpty())
+						return 1;
+					else if(ecePref1.equals(ecePref2)||ecePref1.equals(ecePref3)||ecePref1.equals(ecePref4)||ecePref2.equals(ecePref3)||ecePref2.equals(ecePref4)||ecePref3.equals(ecePref4))
+						return 1;
+					else{
+						
+						Element pr1,pr2,pr3,pr4;
+						pr1 = doc.createElement("ECEPref1");
+						pr2 = doc.createElement("ECEPref2");
+						pr3 = doc.createElement("ECEPref3");
+						pr4 = doc.createElement("ECEPref4");
+						
+						pr1.setTextContent(ecePref1);
+						pr2.setTextContent(ecePref2);
+						pr3.setTextContent(ecePref3);
+						pr4.setTextContent(ecePref4);
+						
+						toadd.appendChild(pr1);
+						toadd.appendChild(pr2);
+						toadd.appendChild(pr3);
+						toadd.appendChild(pr4);
+					}
+				}
+				if(chk2 != null && !chk2.isEmpty()){
+					if(phdCollege.isEmpty()||phdCity.isEmpty()||phdDepartment.isEmpty()||phdDegree.isEmpty()||phdTitle.isEmpty())
+						return 1;
+					else if(phdState.isEmpty()||phdYear.isEmpty())
+						return 1;
+					else{
+						try{
+							Element pname,pcity,pdep,pdeg,pth,pmrks,pstate,pyear;
+							pname = doc.createElement("pgName");
+							pcity = doc.createElement("pgCity");
+							pdep = doc.createElement("pgDepartment");
+							pdeg = doc.createElement("pgDegree");
+							pth = doc.createElement("pgThesis");
+							pmrks = doc.createElement("pgMarks");
+							pstate = doc.createElement("pgState");
+							pyear = doc.createElement("pgYear");
+							
+							pname.setTextContent(phdCollege);
+							pcity.setTextContent(phdCity);
+							pdep.setTextContent(phdDepartment);
+							pdeg.setTextContent(phdDegree);
+							pth.setTextContent(phdTitle);
+							String phdRadio = req.getParameter("phdRadio");
+							if(phdRadio.equals("MARKS"))
+								pmrks.setTextContent(String.valueOf(Double.parseDouble(req.getParameter("phdMARKS"))/Double.parseDouble(req.getParameter("phdFactor").toString())));
+							else
+								pmrks.setTextContent(req.getParameter("phdCGPA"));
+							pstate.setTextContent(phdState);
+							pyear.setTextContent(phdYear);
+							
+							toadd.appendChild(pname);
+							toadd.appendChild(pcity);
+							toadd.appendChild(pdep);
+							toadd.appendChild(pdeg);
+							toadd.appendChild(pth);
+							toadd.appendChild(pmrks);
+							toadd.appendChild(pstate);
+							toadd.appendChild(pyear);
+						}catch(Exception e){
+							return 1;
+						}
+					}
+				}
+				if(chk3 != null && !chk3.isEmpty()){
+					if(examYear.toString().isEmpty()||examName.isEmpty()||examSubject.isEmpty()||examScore.isEmpty()||examRank.isEmpty())
+						return 1;
+					else{
+						Element oexam,osub,oscore,orank,oyear;
+						oexam = doc.createElement("otherExam");
+						osub = doc.createElement("otherSubject");
+						oscore = doc.createElement("otherScore");
+						orank = doc.createElement("otherRank");
+						oyear = doc.createElement("otherYear");
+						
+						oexam.setTextContent(examName);
+						osub.setTextContent(examSubject);
+						oscore.setTextContent(examScore);
+						orank.setTextContent(examRank);
+						oyear.setTextContent(examYear);
+						
+						toadd.appendChild(oexam);
+						toadd.appendChild(osub);
+						toadd.appendChild(oscore);
+						toadd.appendChild(orank);
+						toadd.appendChild(oyear);
+					}
+				}
+				if(chk1 != null && !chk1.isEmpty()){
+					if(gateYear.isEmpty()||gateArea.isEmpty()||gateMarks.isEmpty()||gateScore.isEmpty()||gateRank.isEmpty())
+						return 1;
+					else{
+						try{
+							if(Double.parseDouble(gateMarks)<0 || Double.parseDouble(gateMarks)>1000){
+								return 1;
+							}
+						}catch(Exception e){
+							return 1;
+						}
+						Element garea,gmarks,gscore,grank,gyear;
+						garea = doc.createElement("gateArea");
+						gmarks = doc.createElement("gateMarks");
+						gscore = doc.createElement("gateScore");
+						grank = doc.createElement("gateRank");
+						gyear = doc.createElement("gateYear");
+						garea.setTextContent(gateArea);
+						gmarks.setTextContent(gateMarks);
+						gscore.setTextContent(gateScore);
+						grank.setTextContent(gateRank);
+						gyear.setTextContent(gateYear);
+						
+						toadd.appendChild(garea);
+						toadd.appendChild(gmarks);
+						toadd.appendChild(gscore);
+						toadd.appendChild(grank);
+						toadd.appendChild(gyear);
+						
+					}
+				}
+//				
+				
+				Element xbr,xpr,xiibr,xiipr,deg,dep,cljname,uniname,coljcity,mrks,coljstate,grayear,markdiv,xpass,xiipass,cv,purpose;
+				xbr = doc.createElement("xBoard");
+				xpr = doc.createElement("xPercent");
+				xiibr = doc.createElement("xiiBoard");
+				xiipr = doc.createElement("xiiPercent");
+				deg = doc.createElement("degree");
+				dep = doc.createElement("department");
+				cljname = doc.createElement("coljName");
+				uniname = doc.createElement("universityName");
+				coljcity = doc.createElement("coljCity");
+				mrks = doc.createElement("degreeMarks");
+				coljstate = doc.createElement("coljState");
+				grayear = doc.createElement("graduationYear");
+				markdiv = doc.createElement("markDiv");
+				xpass = doc.createElement("xPassingYear");
+				xiipass = doc.createElement("xiiPassingYear");
+				cv = doc.createElement("cvPath");
+				purpose = doc.createElement("purposePath");
+				
+				xbr.setTextContent(xBoard);
+				xpr.setTextContent(xMarks);
+				xiibr.setTextContent(xiiBoard);
+				xiipr.setTextContent(xiiMarks);
+				deg.setTextContent(gradDegree);
+				dep.setTextContent(gradDepartment);
+				cljname.setTextContent(gCollege);
+				uniname.setTextContent(gUniversity);
+				coljcity.setTextContent(gCity);
+				if(req.getParameter("gCheck").equals("MARKS")){
+					mrks.setTextContent(String.valueOf((Double.parseDouble(req.getParameter("MARKS"))/Double.parseDouble(req.getParameter("gFactor")))));
+				}
+				else{
+					mrks.setTextContent(req.getParameter("CGPA"));
+				}
+				coljstate.setTextContent(gState);
+				grayear.setTextContent(gYear);
+				markdiv.setTextContent(req.getParameter("gFactor"));
+				xpass.setTextContent(xYear);
+				xiipass.setTextContent(xiiYear);
+				
+				cvPath = "hello";
+				purposePath = "hello";
+				
+				cv.setTextContent(cvPath);
+				purpose.setTextContent(purposePath);
+				
+				toadd.appendChild(xbr);
+				toadd.appendChild(xpr);
+				toadd.appendChild(xiibr);
+				toadd.appendChild(xiipr);
+				toadd.appendChild(deg);
+				toadd.appendChild(dep);
+				toadd.appendChild(cljname);
+				toadd.appendChild(uniname);
+				toadd.appendChild(coljcity);
+				toadd.appendChild(coljstate);
+				toadd.appendChild(mrks);
+				toadd.appendChild(grayear);
+				toadd.appendChild(markdiv);
+				toadd.appendChild(xpass);
+				toadd.appendChild(xiipass);
+				toadd.appendChild(cv);
+				toadd.appendChild(purpose);
+				
+				toadd.setAttribute("save2", "1");
+				System.out.println("saved 2");
+				//uploadFiles();
+			}catch(Exception e){
+				return 1;
+			}
+		}
+		}catch(Exception ex1){ return 1;}
+		return 0;
 	}
 	public static int s3(HttpServletRequest req, HttpServletResponse resp){
 		return 1;
@@ -312,18 +578,5 @@ public class Person {
 			}catch(Exception e){
 				System.out.println("omg!!!");
 			}
-	}
-	
-	public static int initS1(HttpServletRequest req){
-		return 1;
-	}
-	public static int initS2(HttpServletRequest req){
-		return 1;
-	}
-	public static int initS3(HttpServletRequest req){
-		return 1;
-	}
-	public static int initS4(HttpServletRequest req){
-		return 1;
 	}
 }
